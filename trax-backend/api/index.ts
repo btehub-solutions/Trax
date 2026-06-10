411d214f1d0ba801d397991c7317b397f220415e
btehub-solutions <btehubsolutions@gmail.com>
2026-06-10 21:24:02 +0100

    chore: set NODE_TLS_REJECT_UNAUTHORIZED globally

--- trax-backend/api/index.ts
+++ trax-backend/api/index.ts
@@ -1,3 +1,4 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
 import { configureApp } from '../src/app.bootstrap';
