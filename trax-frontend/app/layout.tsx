cde8fa8c4d03f78322c0f642110b07abf5de5e1c
btehub-solutions <btehubsolutions@gmail.com>
2026-08-28 15:24:45 +0100

    fix(analytics): standard GA4 gtag config call

--- trax-frontend/app/layout.tsx
+++ trax-frontend/app/layout.tsx
@@ -62,9 +62,7 @@ export default function RootLayout({
                   window.dataLayer = window.dataLayer || [];
                   function gtag(){dataLayer.push(arguments);}
                   gtag('js', new Date());
-                  gtag('config', '${gaId}', {
-                    page_path: window.location.pathname,
-                  });
+                  gtag('config', '${gaId}');
                 `,
               }}
             />
